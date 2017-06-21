9e9b8fb325e373dd3e2b50daa326e1a749771d55
Augusto Moita <a94moita@outlook.com>
2017-06-20 21:15:29 -0300

    Agrego hobbies, edad y comento js no usado

---
+++ edad.js
@@ -0,0 +1,12 @@
+$(function () {
+    var year = 1994
+    var month = 9
+    var day = 6
+    var today = new Date()
+    var age = today.getFullYear()-year
+    age = 
+        (today.getMonth() < month || 
+        (today.getMonth() == month && today.getDate() < day)) 
+        ? --age : age
+    $('#edad').text(age)
+})
